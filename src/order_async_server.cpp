c10d8025f2acddf1ea9b209048ae09f06aaec94a
Dendi Suhubdy <suhubdyd@iro.umontreal.ca>
2019-03-21 14:57:53 +0700

    adding order Asynchronous server

--- src/order_async_server.cpp
+++ src/order_async_server.cpp
@@ -66,13 +66,21 @@ class ServerImpl final {
       if (status_ == CREATE) {
         // Make this instance progress to the PROCESS state.
         status_ = PROCESS;
+        client.connect("127.0.0.1", 6379, [](const std::string& host, std::size_t port, cpp_redis::connect_state status) {
+            if (status == cpp_redis::connect_state::dropped) {
+                std::cout << "client disconnected from " << host << ":" << port << std::endl;
+                }
+        });
+
+        const char * zmq_protocol = "tcp://*:5555";
+        zmq_publisher.bind(zmq_protocol);
 
         // As part of the initial CREATE state, we *request* that the system
-        // start processing SayHello requests. In this request, "this" acts are
+        // start processing SendOrder requests. In this request, "this" acts are
         // the tag uniquely identifying the request (so that different CallData
         // instances can serve different requests concurrently), in this case
         // the memory address of this CallData instance.
-        service_->RequestSayHello(&ctx_, &request_, &responder_, cq_, cq_,
+        service_->RequestSendOrder(&ctx_, &request_, &responder_, cq_, cq_,
                                   this);
       } else if (status_ == PROCESS) {
         // Spawn a new CallData instance to serve new clients while we process
@@ -81,14 +89,87 @@ class ServerImpl final {
         new CallData(service_, cq_);
 
         // The actual processing.
-        std::string prefix("Hello ");
-        reply_.set_message(prefix + request_.name());
+        auto now = std::chrono::system_clock::now();
+        
+        auto itt = std::chrono::system_clock::to_time_t(now);
+        
+        std::ostringstream ss;
+        
+        ss << std::put_time(gmtime(&itt), "%FT%TZ");
+        
+        // requesting value of quantity
+        int quantity = request_.quantity();
+        // requesting value of price
+        double price = request_.price();
+        // requesting type
+        bool type = request_.type();
+        // requesting instrument
+        std::string inst = request_.inst();
+        // requesting user
+        std::string user = request_.user();
+        // requesting pass
+        std::string pass = request_.pass();
+        
+        auto get = client.get(user); 
+        client.sync_commit();
 
-        // And we are done! Let the gRPC runtime know we've finished, using the
-        // memory address of this instance as the uniquely identifying tag for
-        // the event.
-        status_ = FINISH;
-        responder_.Finish(reply_, Status::OK, this);
+        std::string pass_hash = get.get().as_string();
+        
+        if (pass_hash.compare(pass) == 0) {
+            // user authenticated
+            // user can send order to exchange
+            // Check user and password for authentication
+            // database 0 in Redis is used for authentication
+            std::string fix_message = std::to_string(quantity)
+                                      + " "
+                                      + std::to_string(price)
+                                      + " "
+                                      + std::to_string(type)
+                                      + " "
+                                      + inst
+                                      + " "
+                                      + user
+                                      + " "
+                                      + pass;                          
+            // std::cout << fix_message << std::endl;
+            
+            // database 1 is used for orders JSON dumps
+            
+            // Insert orders into Zeromq queue
+            //zmq::message_t zmq_request (sizeof(fix_message));
+            //std::memcpy(zmq_request.data (), fix_message.c_str(), sizeof(fix_message));
+            //zmq_publisher.send(zmq_request);            
+            //s_sendmore (zmq_publisher, inst);
+            //s_send (zmq_publisher, fix_message);       
+            // Dump values to Redis on database 1
+            // We could dumpt the orders into FIX messages strings
+            // client.set(ss.str(), fix_message, [](cpp_redis::reply& reply) {});
+            //client.sync_commit();
+
+            // Show orders
+            std::cout << ss.str()
+                      << " timed. Order consists of "
+                      << "quantity " << quantity
+                      << " price " << price
+                      << " type " << type
+                      << " instrument " << inst
+                      << " username " << user
+                      << " password " << pass
+                      << std::endl; 
+
+            // And we are done! Let the gRPC runtime know we've finished, using the
+            // memory address of this instance as the uniquely identifying tag for
+            // the event.
+            status_ = FINISH;
+            responder_.Finish(reply_, Status::OK, this);
+        } else {
+            // And we are done! Let the gRPC runtime know we've finished, using the
+            // memory address of this instance as the uniquely identifying tag for
+            // the event.
+            status_ = FINISH;
+            responder_.Finish(reply_, Status::OK, this);
+        
+        }
       } else {
         GPR_ASSERT(status_ == FINISH);
         // Once in the FINISH state, deallocate ourselves (CallData).
@@ -108,12 +189,20 @@ class ServerImpl final {
     ServerContext ctx_;
 
     // What we get from the client.
-    HelloRequest request_;
+    OrderRequest request_;
     // What we send back to the client.
-    HelloReply reply_;
+    OrderReply reply_;
 
+    //! Enable logging
+    cpp_redis::active_logger = std::unique_ptr<cpp_redis::logger>(new cpp_redis::logger);
+    // CPP redis database
+    cpp_redis::client client;
+    // Zeromq sockets
+    zmq::context_t zmq_context(1);
+    zmq::socket_t zmq_publisher (zmq_context, ZMQ_PUB);
+    
     // The means to get back to the client.
-    ServerAsyncResponseWriter<HelloReply> responder_;
+    ServerAsyncResponseWriter<OrderReply> responder_;
 
     // Let's implement a tiny state machine with the following states.
     enum CallStatus { CREATE, PROCESS, FINISH };
